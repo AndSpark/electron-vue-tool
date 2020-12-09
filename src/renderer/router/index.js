c24a11840d9b21752f4cc7f61029c61d8f1ae20f
golusa <421786477@qq.com>
2020-12-09 23:08:14 +0800

    productUpload start

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 const Home = () => import('@/view/Home')
 const ImgResize = () => import('@/tool/imgResize/g-imgResize')
+const ProductUpload = () => import('@/tool/productUpload/g-productUpload')
 export default new Router({
   routes: [
     {
@@ -15,6 +16,11 @@ export default new Router({
       path: '/imgresize',
       component: ImgResize,
       name: '图片处理工具',
+    },
+    {
+      path: '/productupload',
+      component: ProductUpload,
+      name: '产品上传工具',
     },
     {
       path: '*',
---
+++ src/renderer/tool/productUpload/g-productUpload.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+#g-productUpload
+  v-container
+    v-row
+      v-btn 打开文件夹
+    v-sheet.my-5.mx-n5
+      v-treeview(
+        v-model="tree"
+        :open="initiallyOpen"
+        :items="items"
+        activatable
+        item-key="name"
+        open-on-click
+        transition
+        @update:active="show"
+      )
+        template(v-slot:prepend="{item,open}")
+          v-icon(v-if="!item.file").
+            {{open ? 'mdi-folder-open' : 'mdi-folder'}}
+          v-icon(v-else).
+            {{files[item.file]}}
+</template>
+
+<script>
+export default {
+  name: 'g-productUpload',
+  components: {},
+  props: {},
+  data() {
+    return {
+      initiallyOpen: ['public'],
+      files: {
+        html: 'mdi-language-html5',
+        js: 'mdi-nodejs',
+        json: 'mdi-code-json',
+        md: 'mdi-language-markdown',
+        pdf: 'mdi-file-pdf',
+        png: 'mdi-file-image',
+        txt: 'mdi-file-document-outline',
+        xls: 'mdi-file-excel',
+      },
+      tree: [],
+      items: [
+        {
+          name: '.git',
+        },
+        {
+          name: 'node_modules',
+        },
+        {
+          name: 'public',
+          children: [
+            {
+              name: 'static',
+              children: [
+                {
+                  name: 'logo.png',
+                  file: 'png',
+                },
+              ],
+            },
+            {
+              name: 'favicon.ico',
+              file: 'png',
+            },
+            {
+              name: 'index.html',
+              file: 'html',
+            },
+          ],
+        },
+        {
+          name: '.gitignore',
+          file: 'txt',
+        },
+        {
+          name: 'babel.config.js',
+          file: 'js',
+        },
+        {
+          name: 'vue.config.js',
+          file: 'js',
+        },
+        {
+          name: 'yarn.lock',
+          file: 'txt',
+        },
+      ],
+    }
+  },
+  computed: {},
+  methods: {
+    show(arr) {
+      console.log(arr)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+#g-productUpload {
+}
+</style>
